21d3018dbbc4adcf0fd0f1b765a4a725c392a7ac
Jan Sorgalla <jsorgalla@gmail.com>
2016-01-26 09:54:02 +0100

    PHP 5.3 compat

--- src/Exception/UnknownDefinitionException.php
+++ src/Exception/UnknownDefinitionException.php
@@ -6,9 +6,21 @@ class UnknownDefinitionException extends \RuntimeException
 {
     public static function create($def)
     {
-        return new self(sprintf(
-            'Unknown CRS definition %s',
-            json_encode($def, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
-        ));
+        $jsonFlags = 0;
+
+        if (defined('JSON_UNESCAPED_SLASHES')) {
+            $jsonFlags |= JSON_UNESCAPED_SLASHES;
+        }
+
+        if (defined('JSON_UNESCAPED_UNICODE')) {
+            $jsonFlags |= JSON_UNESCAPED_UNICODE;
+        }
+
+        return new self(
+            sprintf(
+                'Unknown CRS definition %s',
+                json_encode($def, $jsonFlags)
+            )
+        );
     }
 }
